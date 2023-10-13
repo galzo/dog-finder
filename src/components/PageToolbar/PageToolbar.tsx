0b437e21665f7d62f9906fd3a0e9c62bdf9b84c7
Gal Zohar <galz@ai21.com>
2023-10-13 16:34:29 +0300

    added tabler icons

--- src/components/PageToolbar/PageToolbar.tsx
+++ src/components/PageToolbar/PageToolbar.tsx
@@ -7,6 +7,12 @@ const usePageToolbarStyles = createStyleHook((theme) => {
       width: "100%",
       height: "70px",
       backgroundColor: theme.palette.background.paper,
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      boxShadow:
+        "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;",
     },
   };
 });
