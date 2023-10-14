3c9cfd1f06dd771b95e545fb7ea761183c806750
EdenGottlieb <gsi12344321@gmail.com>
2023-10-14 18:05:25 +0300

    Add Auth0 integration (#3)
    * Add Auth0 integration
    
    The login/logout and user details is terrible rn, but it works
    
    * Add useGetServerApi
    
    usage:
    
    const getServerApi = useGetServerApi();
    const serverApi = await getServerApi()
    const response =await  serverApi.getLostDogsList().json() // Or anything else

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,20 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dog-finder.eu.auth0.com"
+    clientId="8AQ2bGZCSUHQ41Et9f139z6aabFNanxG"
+    authorizationParams={{
+      redirect_uri: window.location.href
+    }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
