3bea0c4116488460aa027e93cf99130283bd27be
Tslil Shani <tslilshani@Tslils-MacBook-Pro.local>
2023-10-17 09:44:54 +0300

    fix: results image

--- src/components/resultsComponents/DogCard.tsx
+++ src/components/resultsComponents/DogCard.tsx
@@ -3,14 +3,11 @@ import { AppTexts } from "../../consts/texts";
 import { Dog } from "./ResultsGrid";
 
 export const DogCard = ({ dog }: { dog: Dog }) => {
-  const image = dog.image;
-  const blob = new Blob([image], { type: "image/*" });
-  const urlCreator = window.URL || window.webkitURL;
-  const imageUrl = urlCreator.createObjectURL(blob);
+  const image = `data:image/jpeg;base64,${dog.image}`;
   return (
     <Card dir="rtl">
       <CardMedia
-        image={imageUrl}
+        image={image}
         component="img"
         style={{ objectFit: "contain" }}
         title="Dog Image"
