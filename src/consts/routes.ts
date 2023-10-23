8851e30cedebcc0c3ac5ceeb0580d72e07b7ded0
YanivWein24 <yanivwein22@gmail.com>
2023-10-23 16:06:41 +0300

    refactor routes in app.tsx to make it cleaner

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -1,11 +1,57 @@
+import { FC } from "react";
+import { DogType } from "../facades/payload.types";
+import { HomePage } from "./../pages/root/HomePage";
+import { ReportDogPage } from "../pages/dogs/ReportDogPage";
+import { SearchDogPage } from "../pages/dogs/SearchDogPage";
+import { ResultsDogPage } from "../pages/dogs/ResultsDogPage";
+
 export const AppRoutes = {
-  root: "/dog-finder",
-  dogs: {
-    report: "/dog-finder/dogs/report",
-    reportFound: "/dog-finder/dogs/report-found",
-    reportLost: "/dog-finder/dogs/report-missing",
-    searchLostDog: "/dog-finder/dogs/search-lost",
-    searchFoundDog: "/dog-finder/dogs/search-found",
-    results: "/dog-finder/dogs/results/:dogType",
-  },
+    root: "/dog-finder",
+    dogs: {
+        report: "/dog-finder/dogs/report",
+        reportFound: "/dog-finder/dogs/report-found",
+        reportLost: "/dog-finder/dogs/report-missing",
+        searchLostDog: "/dog-finder/dogs/search-lost",
+        searchFoundDog: "/dog-finder/dogs/search-found",
+        results: "/dog-finder/dogs/results/:dogType",
+    },
 };
+
+type RouteElement = () => JSX.Element;
+
+interface Route {
+    path: string;
+    element: RouteElement | FC<any>;
+    props?: Record<any, any>;
+}
+
+export const routesWithElements: Route[] = [
+    {
+        path: AppRoutes.root,
+        element: HomePage,
+    },
+    {
+        path: AppRoutes.dogs.reportLost,
+        element: ReportDogPage as RouteElement,
+        props: { dogType: DogType.LOST },
+    },
+    {
+        path: AppRoutes.dogs.reportFound,
+        element: ReportDogPage as RouteElement,
+        props: { dogType: DogType.FOUND },
+    },
+    {
+        path: AppRoutes.dogs.searchLostDog,
+        element: SearchDogPage as RouteElement,
+        props: { dogType: DogType.LOST },
+    },
+    {
+        path: AppRoutes.dogs.searchFoundDog,
+        element: SearchDogPage as RouteElement,
+        props: { dogType: DogType.FOUND },
+    },
+    {
+        path: AppRoutes.dogs.results,
+        element: ResultsDogPage,
+    },
+];
